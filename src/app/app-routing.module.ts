e1142a8f2e85ef4b5a67025af76ac047eae39a9b
odukoyaayodeji@gmail.com <odukoyaayodeji@gmail.com>
2020-03-10 17:05:00 +0100

    prototype development

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'user-details', component: UserDetailsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
